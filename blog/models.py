da3cd9156ef0ca8404f26295b47da838aa741cbc
jaykanjariya <jaykanjariya28@gmail.com>
2019-02-25 22:19:33 +0530

    create templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to = 'images/')
+    body = models.TextField()
