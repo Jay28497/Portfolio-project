5d9a69b6dd074c25409cd264a4d23f4572209aea
jaykanjariya <jaykanjariya28@gmail.com>
2019-03-10 14:50:44 +0530

    Edit project

--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
+    title = models.CharField(max_length = 100)
     image = models.ImageField(upload_to = 'images/')
-    summary = models.CharField(max_length = 200)
+    summary = models.CharField(max_length = 500)
+
+
+    def data(self):
+        return self.summary[:100]
